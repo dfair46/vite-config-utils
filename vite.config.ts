61e3304435d8d5b4a58d55bce9cee8a953561cae
dfair_rika <55684527+dfair46@users.noreply.github.com>
2025-03-10 09:33:24 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import legacy from '@vitejs/plugin-legacy'
+
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig(() => {
+        return {
+            plugins: [
+              vue(), dts(),
+              // targets 目标最低浏览器版本   |    renderLegacyChunks  build时是否需要触发编译器
+              legacy({targets: ['chrome < 60', 'edge < 15'],renderLegacyChunks: false,})
+            ],
+            resolve: {
+                alias: {
+                    // @ts-ignore
+                    '@': path.resolve(__dirname, 'src')
+                }
+            }
+        }
+    }
+);
