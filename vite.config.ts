3332ffeea3e34e6771bc399c4abb592fd39ae34a
dfair_rika <55684527+dfair46@users.noreply.github.com>
2025-03-10 09:38:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,28 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(() => {
         return {
-            plugins: [
-              vue(), dts(),
-              // targets 目标最低浏览器版本   |    renderLegacyChunks  build时是否需要触发编译器
-              legacy({targets: ['chrome < 60', 'edge < 15'],renderLegacyChunks: false,})
-            ],
-            resolve: {
-                alias: {
-                    // @ts-ignore
-                    '@': path.resolve(__dirname, 'src')
-                }
-            }
+                plugins: [
+                      vue(), dts(),
+                      // targets 目标最低浏览器版本   |    renderLegacyChunks  build时是否需要触发编译器
+                      legacy({targets: ['chrome < 60', 'edge < 15'],renderLegacyChunks: false,})
+                    ],
+                
+                resolve: {
+                        alias: {
+                            // @ts-ignore
+                            '@': path.resolve(__dirname, 'src')
+                        }
+                },
+
+                // 移动端像素转化css的fn自动导入
+                css: {
+                    preprocessorOptions: {
+                        scss: {
+                            additionalData: '@import "@/assets/color.scss";'
+                        }
+                    }
+                },
+                
         }
     }
 );
